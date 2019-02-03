ef07c4270f9d90a4ef9df82ccfe6abe46e3f888a
Zanoni Guevara <zanoni.guevara@gmail.com>
2019-02-03 03:32:48 -0400

    funciona

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import * as Plyr from 'plyr';
+import * as Draggable from 'draggable';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  conten: Draggable;
+  videocontenedor:any;
+  player: any;
+  opciones: any;
+
+  ngOnInit() {
+    this.player = new Plyr('#plyrID', { captions: { active: true } });
+    this.videocontenedor = document.getElementById('move');
+    this.opciones = {
+        setCursor: true
+        // onDrag: function (element, x, y) {
+          // 	labelY.innerHTML = y;
+          // }
+          // 	labelX.innerHTML = x;
+      };
+  }
+  pip() {
+    this.conten = new Draggable(this.videocontenedor, this.opciones);
+  }
+  pipno() {
+    // console.log(this.conten.get.x());
+    this.conten.destroy();
+    this.videocontenedor.style.position = "initial";
+  }
+}
